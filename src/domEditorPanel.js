1191513e5a64bfbe72841a5702d231396b1a69df
hossein <hossien.alipour@gmail.com>
2020-08-06 16:38:37 +0000

    init

---
+++ src/domEditorPanel.js
@@ -0,0 +1,25 @@
+// function createPanel(el) {
+
+
+
+//   for (let att of el.attributes) {
+
+//     if (att.value == 'true' || att.value == 'false') {
+//       let checkbox = document.createElement('input');
+//       checkbox.setAttribute('type', 'checkbox');
+//       checkbox.setAttribute('name', att.name);
+//       checkbox.setAttribute('value', att.value);
+
+//     }
+
+//     switch (att.name) {
+//       case 'class':
+
+//     }
+
+
+
+//   }
+
+
+// }
