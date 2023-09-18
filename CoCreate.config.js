34cd69094c8a50004b8dafff4b413927dcd25cc1
frankpagan <frank@cocreate.app>
2023-09-17 19:11:35 -0600

    fix:  Add path and pathname

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -8,17 +8,16 @@ module.exports = {
             "object": {
                 "_id": "6019b2844eea0817df303b06",
                 "name": "index.html",
-                "path": "/docs/attributes/index.html",
+                "path": "/docs/attributes",
+                "pathname": "/docs/attributes/index.html",
                 "src": "{{./docs/index.html}}",
                 "host": [
                     "*",
                     "general.cocreate.app"
                 ],
-                "directory": "/docs/attributes",
-                "parentDirectory": "{{parentDirectory}}",
+                "directory": "attributes",
                 "content-type": "{{content-type}}",
-                "public": "true",
-                "website_id": "644d4bff8036fb9d1d1fd69c"
+                "public": "true"
             }
         }
     ]
